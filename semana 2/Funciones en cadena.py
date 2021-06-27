afd20114a57c400be5d514dea0efd1c5ea28ba18
jacostac5 <85777249+jacostac5@users.noreply.github.com>
2021-06-27 10:14:25 -0500

    Add files via upload

---
+++ semana 2/Funciones en cadena.py
@@ -0,0 +1,8 @@
+#funciones de cadenas
+mensaje = 'Bienvenidos' + ' usuarios ' + 'Programa de python'
+men1=mensaje.split()
+men2=' '.join(men1)
+print(mensaje[0],mensaje[0:4],men1,men2)
+print(mensaje.find("a mi nuevo"), mensaje.lower())
+
+
